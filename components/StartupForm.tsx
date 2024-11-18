23a8c50943d9bc8caf59982bab390bb93c95c4b8
Ayush negi <87976275+yuyuayush@users.noreply.github.com>
2024-11-18 23:48:49 +0530

    add

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -71,7 +71,7 @@ const StartupForm = () => {
     }
   };
 
-  const [state, formAction, isPending] = useActionState(handleFormSubmit, {
+  const [formAction, isPending] = useActionState(handleFormSubmit, {
     error: "",
     status: "INITIAL",
   });
